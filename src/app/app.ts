9c31f05a33937813f0ee88421b61517d77b6abb9
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-02-10 12:41:49 -0300

    feat: Adicionando seção de Início

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
+import { Start } from './start/start';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Start],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
